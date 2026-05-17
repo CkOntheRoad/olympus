465f2a3adf8d86a866a2df5899530ee335f51870
Christian Kyster <AtHome@MacBook-Pro.fritz.box>
2026-05-17 10:17:25 +0200

    new tesla page

---
+++ src/app/tesla/page.tsx
@@ -0,0 +1,9 @@
+export default function TeslaReport() {
+  return (
+    <iframe
+      src="/tesla-risk-report.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      title="TSLA Risk Report"
+    />
+  );
+}
